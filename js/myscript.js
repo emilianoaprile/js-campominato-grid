c0abd65a9a515b19cab94d78a8adf8f22c14d7e0
EmilianoAprile <emilianoaprile77@icloud.com>
2023-10-14 17:27:15 +0200

    ADDED - event click: it shows grid

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,46 @@
 console.log('CAMPOMINATO')
 
+// richiamo elemento btn play dal DOM
 const btnGiocaElement = document.querySelector('.btn-play');
 console.log(btnGiocaElement);
 
+// richiamo elemento grid dal DOM
 const gridElement = document.querySelector('.grid');
 console.log(gridElement);
 
-for (let i = 0; i < 100; i++) {
-    const cellNumber = i + 1;
-    console.log(cellNumber);
+// creo evento click per far comparire griglia
 
-    const htmlCell = document.createElement('div');
-    console.log(htmlCell);
+btnGiocaElement.addEventListener('click', function () {
+    console.log('gioca', btnGiocaElement);
 
-    gridElement.appendChild(htmlCell);
-    htmlCell.classList.add('grid-cell')
+    // assegno stringa vuota in modo tale che ad ogni click non si generano griglie multiple
+    gridElement.innerHTML = '';
 
-    htmlCell.innerHTML = cellNumber;
+    // tolgo la classe none a grid element
+    gridElement.classList.remove('d-none');
 
-}
+    // for per creare le 100 celle ed assegnarli il numero statico
+    for (let i = 0; i < 100; i++) {
+        const cellNumber = i + 1;
+        console.log(cellNumber);
+
+        const htmlCell = document.createElement('div');
+        console.log(htmlCell);
+
+        gridElement.appendChild(htmlCell);
+        htmlCell.classList.add('grid-cell')
+
+        htmlCell.innerHTML = cellNumber;
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
